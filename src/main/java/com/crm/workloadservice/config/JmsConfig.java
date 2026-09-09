8ef4c709caae82c4107ff49eb905a978b0d94bcf
kutman_mukarapov <kutman2002mukarapov@gmail.com>
2026-09-09 22:44:26 +0600

    consumes workload messages from activeMQ instead of REST

---
+++ src/main/java/com/crm/workloadservice/config/JmsConfig.java
@@ -0,0 +1,74 @@
+package com.crm.workloadservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import jakarta.jms.ConnectionFactory;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.user}")
+    private String user;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+    @Value("${workload.queue.max-redeliveries:3}")
+    private int maxRedeliveries;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(user, password, brokerUrl);
+
+        RedeliveryPolicy policy = new RedeliveryPolicy();
+        policy.setMaximumRedeliveries(maxRedeliveries);
+        policy.setInitialRedeliveryDelay(1000);
+        policy.setUseExponentialBackOff(true);
+        factory.setRedeliveryPolicy(policy);
+
+        return new CachingConnectionFactory(factory);
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(objectMapper);
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        converter.setTypeIdMappings(
+                java.util.Map.of("TrainerWorkloadRequest", com.crm.workloadservice.dto.TrainerWorkloadRequest.class)
+        );
+        return converter;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
+            ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        factory.setSessionTransacted(true);
+        return factory;
+    }
+}
